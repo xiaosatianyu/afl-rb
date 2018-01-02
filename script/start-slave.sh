42284d3bc59a86f0c9c06a643c26544295ad6d35
xiaosatianyu <xiaosatianyu@126.com>
2018-01-02 16:07:27 +0800

    添加测试启动脚步

--- script/start-slave.sh
+++
@@ -1,1 +0,0 @@
- ../afl-fuzz -i ./seed  -o /tmp/output-rb  -r  -q 2   -Q  -m 8G  -S $1   -- ./86_pwn06-1
---
+++ script/startafl-para.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+AFL_FUZZ="/home/binzhang/git/afl-rb/afl-fuzz"
+SEED="/home/binzhang/git/afl-rb/script/seed"
+OUT_PUT="/tmp/out-aflpara"
+EXE_STR="/home/binzhang/Desktop/tiff2pdf/tiff2pdf @@ -o /dev/null"
+
+rm -rf  $OUT_PUT
+mkdir -p $OUT_PUT
+
+if [ "$1" = "0" ]; then
+$AFL_FUZZ -i $SEED -o $OUT_PUT  -m none -M master  $EXE_STR 
+else
+$AFL_FUZZ -i $SEED -o $OUT_PUT  -m none -S $1  $EXE_STR    
+fi  
