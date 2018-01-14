6f32b4e311fa0b71c16e64aa0d0d374a4c23ccde
xiaosatianyu@126.com <xiaosatianyu@126.com>
2018-01-14 23:01:12 +0800

    增加afl对比执行程序

--- testenv/runaflrb.sh
+++ testenv/runaflrb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TARGET=tiff2pdf-distance-noasan
-AFL_HOME=../binary
+AFL_HOME=../afl-rb
 INPUT=`pwd`/input
 OUTPUT=`pwd`/output-aflrb
 
@@ -32,5 +32,5 @@ if [ ! -f $REAL_TARGET ]; then
 fi
 
 echo $REAL_TARGET
-#$AFL_HOME/fairfuzz -m none -i $INPUT -d -o $OUTPUT -r -q 2 $REAL_TARGET -o /dev/null @@
-gdb --args $AFL_HOME/fairfuzz -m none -i $INPUT  -o $OUTPUT  -d -r -q 2 $REAL_TARGET -o /dev/null @@
+#$AFL_HOME/afl-fuzz -m none -i $INPUT -d -o $OUTPUT -r -q 2 $REAL_TARGET -o /dev/null @@
+gdb --args $AFL_HOME/afl-fuzz -m none -i $INPUT  -o $OUTPUT  -d -r -q 2 $REAL_TARGET -o /dev/null @@
