254fb45efcdeb3e6fb932dcc650088b28e06988c
xiaosatianyu@126.com <xiaosatianyu@126.com>
2018-01-17 14:40:56 +0800

    搭建编译环境,修改运行脚本

--- testenv/startcmp.sh
+++ testenv/startcmp.sh
@@ -1,4 +1,7 @@
-x-terminal-emulator -e "`pwd`/runrdfuzz.sh "  &
-x-terminal-emulator -e "`pwd`/runaflrb.sh "   &
-x-terminal-emulator -e "`pwd`/runaflgo.sh "   &
-x-terminal-emulator -e "`pwd`/runafl2.5.2.sh "   &
+
+target="/home/xiaosatianyu/workspace/git/fuzz/rdfuzz/testenv/binary-test/CVE-2017-5969/xmllint --valid --recover @@"
+
+x-terminal-emulator -e "`pwd`/runrdfuzz.sh $target"   &
+x-terminal-emulator -e "`pwd`/runaflrb.sh $target"   &
+x-terminal-emulator -e "`pwd`/runaflgo.sh $target"   &
+x-terminal-emulator -e "`pwd`/runafl2.5.2.sh $target"   &
